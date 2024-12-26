95ac8e5cbaab785e1270f328c058cce5913e3de0
Bhargavi Bairagoni <113205911+bhargavibairagoni@users.noreply.github.com>
2024-12-26 11:17:30 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -4,7 +4,7 @@ sudo apt install git -y
 sudo apt install python3-pip -y
 sudo apt install pip3 -y
 cd /home/ubuntu/
-git clone https://github.com/bhargavibairagoni/flight-perdiction.git
-cd /home/ubuntu/flight-perdiction
+git clone https://github.com/bhargavibairagoni/Medical-Insurance.git
+cd /home/ubuntu/Medical-Insurance
 pip3 install -r requirements.txt
 python3 app.py
