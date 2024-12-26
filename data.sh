a010430fcf545180b49121fdfa1f683410035bc8
Bhargavi Bairagoni <113205911+bhargavibairagoni@users.noreply.github.com>
2024-12-26 10:39:15 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -3,7 +3,6 @@ sudo apt update -y
 sudo apt install git -y
 sudo apt install python3-pip -y
 sudo apt install pip3 -y
-mkdir /bhargavi
 cd /home/ubuntu/
 git clone https://github.com/bhargavibairagoni/indian-liver-patients.git
 cd /indian-liver-patients
