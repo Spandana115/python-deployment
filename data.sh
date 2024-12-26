57e399693c87d484211d99cd6ba3ed35efad1bd0
Bhargavi Bairagoni <113205911+bhargavibairagoni@users.noreply.github.com>
2024-12-26 10:51:06 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -5,6 +5,6 @@ sudo apt install python3-pip -y
 sudo apt install pip3 -y
 cd /home/ubuntu/
 git clone https://github.com/bhargavibairagoni/indian-liver-patients.git
-cd /indian-liver-patients
+cd /home/ubuntu/indian-liver-patients
 pip3 install -r requirements.txt
 python3 app.py
