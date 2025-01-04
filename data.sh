ae5687eedb7f043723576d0d90299592b09fd520
Bhargavi Bairagoni <113205911+bhargavibairagoni@users.noreply.github.com>
2025-01-04 15:24:47 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install git -y
-sudo apt install python3-pip -y
-sudo apt install pip3 -y
-cd /home/ubuntu/
+
+
+#sudo apt update -y
+#sudo apt install git -y
+#sudo apt install python3-pip -y
+#sudo apt install pip3 -y
+#cd /home/ubuntu/
+
+
 git clone https://github.com/bhargavibairagoni/Medical-Insurance.git
 cd Medical-Insurance
 pip3 install -r requirements.txt
